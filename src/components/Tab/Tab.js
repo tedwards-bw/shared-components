64d9461e5155836a109e3c1db59c5cc4b188fc78
Tony Edwards <tedwards@bandwidth.com>
2019-02-28 14:39:36 -0500

    Just use default flex container behavior

--- src/components/Tab/Tab.js
+++ src/components/Tab/Tab.js
@@ -88,10 +88,6 @@ const Tab = styled.li`
     }
   }}
 
-  ${styles.List}:not(${styles.List.Vertical}) > & {
-    flex: 0 0 auto;
-  }
-
   ${styles.List.Vertical} > &, ${styles.Container.Vertical} > ${
   styles.List
 } > & {
